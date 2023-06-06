3b593241a3e3e507c8acc7618a2fd34e6c52b7df
dnozaki <dnozaki88888888@gmail.com>
2023-06-06 15:08:53 +0900

    static-members

---
+++ src/static-members.ts
@@ -0,0 +1,19 @@
+export {};
+
+class Me {
+    static isProgrammer: boolean = true;
+    static firstName: string = 'Atsushi';
+    static lastName: string = 'Ishida';
+
+    static work() {
+        // return "Hey, guys! This is Atsushi! Are you interested in TypeScript? Let's dive into TypeScript!";
+        return `Hey, guys! This is ${this.firstName}. Are you interested in TypeScript? Let's dive into TypeScript!`;
+    }
+}
+
+// let me = new Me();
+// console.log(me.isProgrammer);
+
+// インスタンスを作らなくてもプロパティにアクセスできる
+console.log(Me.isProgrammer);
+console.log(Me.work());
