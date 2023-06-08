6a341f92704ee32a8aed7230cbdf1499a6a3a1dc
dnozaki <dnozaki88888888@gmail.com>
2023-06-08 16:48:09 +0900

    pick and omit

--- src/exclude-extract-nonnullable-definition.ts
+++ src/exclude-extract-nonnullable-definition.ts
@@ -1,9 +1,9 @@
 export {};
 
 type MyExclude = 
-  (string extends string | number ? never : string)
-| (number extends string | number ? never : number)
-| (DebugType extends string | number ? never : DebugType);
+  (string extends string | number ? never : string) // never になる
+| (number extends string | number ? never : number) // never になる
+| (DebugType extends string | number ? never : DebugType); // faulse になってDebugType が残る
 type DebugType = () => void;
 type SomeTypes = string | number | DebugType;
 type FunctionType = Exclude<SomeTypes, string | number>;
